47dbd7d9fdb27fb8c9d502b796b6349b5128f539
enochgao <enochgao@qq.com>
2024-05-07 09:16:06 +0800

    chore: v19

--- apps/host/src/app/app.module.ts
+++ apps/host/src/app/app.module.ts
@@ -6,20 +6,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { BaseService } from 'my-base';
-import { NzIconService } from 'ng-zorro-antd/icon';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
   bootstrap: [AppComponent],
 })
 export class AppModule {
-  constructor(
-    private valueService: BaseService,
-    private nzIconService: NzIconService
-  ) {
+  constructor(private valueService: BaseService) {
     this.valueService.changeAssetsSource('https://nx.dev');
-    this.nzIconService.changeAssetsSource(
-      'https://winkong-frontend.oss-cn-qingdao.aliyuncs.com/ng-zorro-icons'
-    );
   }
 }
