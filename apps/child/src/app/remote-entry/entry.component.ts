0b218fbacd8d391430de86c418b37bbfb2f49c2a
enochgao <enochgao@qq.com>
2024-09-11 15:48:54 +0800

    chore: chore

--- apps/child/src/app/remote-entry/entry.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'nx-test-child-entry',
-  template: ` child `,
-})
-export class RemoteEntryComponent {}
--- apps/host/src/app/lazy/lazy.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'nx-test-lazy',
-  template: ` host `,
-})
-export class LazyComponent {}
